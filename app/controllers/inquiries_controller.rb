ba8403230b63080362b17465c83e0df0095901d3
John <s.johnwatson@gmail.com>
2013-12-30 14:32:43 -0500

    Inquiry can now be created with happy path

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -3,4 +3,23 @@ class InquiriesController < ApplicationController
   def index
 
   end
+
+  def new
+    @inquiry = Inquiry.new
+  end
+
+  def create
+    @inquiry = Inquiry.new(inquiry_params)
+
+    if @inquiry.save
+      redirect_to inquiries_path, notice: 'Inquiry created successfully'
+    end
+  end
+
+  private
+  def inquiry_params
+    params.require(:inquiry).permit(:first_name,
+        :last_name, :email, :subject, :description)
+  end
+
 end
